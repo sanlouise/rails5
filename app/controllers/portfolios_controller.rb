86b702bee9f2e1d4879bcc7fda159bd8e6f4d830
sanlouise <sandra.adamshallie@gmail.com>
2017-05-29 20:52:02 -0400

    Update destroy method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,10 +41,11 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
   
