87daf51064ec04ee7a2881afa8b901155ff35211
sanlouise <sandra.adamshallie@gmail.com>
2017-06-07 13:46:36 +0200

    Add default values for portfolio items, after_initialize method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,12 @@ class Portfolio < ApplicationRecord
   scope :web_app_portfolio_items, -> { where(subtitle: 'Ruby on Rails') &&  where(subtitle: 'Javascript')}
   scope :mobile_app_portfolio_items, -> { where(subtitle: 'Mobile App') }
   
+  # Occurs right after 'new' method in controller has run.
+  after_initialize :set_defaults
+  #Note, 'after_create' happens after 'create' method has run.
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
