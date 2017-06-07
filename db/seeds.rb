02f2848432e073647cb4f1b432f074ab8fab6638
sanlouise <sandra.adamshallie@gmail.com>
2017-06-07 11:20:52 +0200

    Fix typo and update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ end
 10.times do |blog|
   Blog.create!(
     title: "Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     topic_id: Topic.last.id
   )
 end
@@ -19,10 +19,20 @@ end
   )
 end
 
-9.times do |item|
+6.times do |item|
   Portfolio.create!(
     title: "Title #{item}",
-    subtitle: "This is an amazing portfolio item",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+  )
+end
+
+3.times do |item|
+  Portfolio.create!(
+    title: "Title #{item}",
+    subtitle: "React",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200",
