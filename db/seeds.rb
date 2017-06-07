900631684722698c7307f51a2625678de2c2c25a
sanlouise <sandra.adamshallie@gmail.com>
2017-06-07 11:18:45 +0200

    Update seeds file with topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(
+    title: "#{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
     title: "Blog Post #{blog}",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    topic_id: Topic.last.id
   )
 end
 
